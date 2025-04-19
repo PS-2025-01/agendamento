d46b0eb6f7df7f32b005b49b6ef05ed5fed3f6e3
Fabiano Lima <fabianosl@ufrrj.br>
2025-04-18 22:13:06 -0300

    feat(usuario): modelo do usuario e findByEmail

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { EspecialidadesModule } from './especialidades/especialidades.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { options } from './data-source';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(options), EspecialidadesModule],
+  imports: [
+    TypeOrmModule.forRoot(options),
+    EspecialidadesModule,
+    UsuarioModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ backend/src/usuario/entities/tipoUsuario.enum.ts
@@ -0,0 +1,5 @@
+export enum TipoUsuario {
+  ADMIN = 'admin',
+  PACIENTE = 'paciente',
+  MEDICO = 'medico',
+}
