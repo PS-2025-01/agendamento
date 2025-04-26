87719f3ab644300a0b6585ce80a9f90a8dc08f78
Carlos Gabriel <83463297+carlosgabriell@users.noreply.github.com>
2025-04-26 11:38:17 -0300

    Update create-especialidade.dto.ts
    feat(dto): adiciona validações no CreateEspecialidadeDto
    
    - Garante que o campo "nome" seja uma string e não esteja vazio usando class-validator.
    - Melhora a segurança e a confiabilidade dos dados recebidos na criação de especialidades.

--- backend/src/especialidades/dto/create-especialidade.dto.ts
+++ backend/src/especialidades/dto/create-especialidade.dto.ts
@@ -1,3 +1,7 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateEspecialidadeDto {
+  @IsString({ message: 'O nome precisa ser uma string.' })
+  @IsNotEmpty({ message: 'O nome é obrigatório.' })
   nome: string;
 }
