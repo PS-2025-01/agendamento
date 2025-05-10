4017550235917e5b65e864c2d80ef313754c8ebb
Lucyan Ovidio <lucyanovidio@MacBook-Pro-de-Lucyan.local>
2025-05-10 14:32:24 -0300

    feat: iniciando rotas com react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,16 @@
-const App = () => {
+import React from "react";
+import AppRoutes from "./routes";
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+
+function App() {
   return (
-    <div className="login-container">
-      <h1>MediAgenda</h1>
-
-      <form action="#" className="login-form">
-        <h3>Entrar</h3>
-
-        <div className="input-wrapper">
-          <label htmlFor="email">E-mail:</label>
-          <input type="email" placeholder="usuario@exemplo.com" className="input-field" name="email" required />
-        </div>
-
-        <div className="input-wrapper">
-          <label htmlFor="password">Senha:</label>
-          <input type="password" placeholder="••••••" className="input-field" name="password" required />
-        </div>
-
-        <input type="submit" value="Login" className="submit-btn" />
-        <a href="#" className="forgot-pass-link">Esqueci minha senha</a>
-      </form>
-    </div>
-  )
+    <>
+      {/* <Header /> */}
+      <AppRoutes />
+      {/* <Footer /> */}
+    </>
+  );
 }
 
-export default App
+export default App;
