b1c695a519761a0e059d025ce157e6364b1a0da0
Fabiano Lima <fabianosl@ufrrj.br>
2025-05-20 21:42:49 -0300

    chore: usando variaveis de ambiente

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,12 +1,13 @@
+import 'dotenv/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const options: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'agendamento',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*'],
   synchronize: false,
