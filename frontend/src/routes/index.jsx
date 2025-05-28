5d470cb1187e682e17c1a9e87a64b4d212ede368
Mariana Correa <marianacorrea0304@gmail.com>
2025-05-28 14:15:28 -0300

    Home Medico versao1

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "../pages/login/Login";
 import Signup from "../pages/Signup/Signup";
+import HomeMed from "../pages/medico/HomeMed";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function AppRoutes() {
@@ -9,6 +10,8 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={<Login />}></Route>
                 <Route path="/signup" element={<Signup />}></Route>
+                <Route path="/home-med" element={<HomeMed />}></Route>
+                
             </Routes>
         </Router>
     );
