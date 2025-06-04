a26d4ec33b7cdacbad947ff4b76d1214b47ab816
fabianosl1 <fabianosl@ufrrj.br>
2025-06-04 16:54:34 -0300

    chore: format

--- backend/src/modules/especialidades/dto/especialidade.dto.ts
+++ backend/src/modules/especialidades/dto/especialidade.dto.ts
@@ -1,15 +1,15 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Especialidade } from "../entities/especialidade.entity";
+import { ApiProperty } from '@nestjs/swagger';
+import { Especialidade } from '../entities/especialidade.entity';
 
 export class EspecialidadeDto {
-    @ApiProperty({ example: 1 })
-    id: number;
-    
-    @ApiProperty({ example: 'cardiologista' })
-    nome: string;
+  @ApiProperty({ example: 1 })
+  id: number;
 
-    constructor(especialidade: Especialidade) {
-        this.id = especialidade.id;
-        this.nome = especialidade.nome;
-    }
+  @ApiProperty({ example: 'cardiologista' })
+  nome: string;
+
+  constructor(especialidade: Especialidade) {
+    this.id = especialidade.id;
+    this.nome = especialidade.nome;
+  }
 }
