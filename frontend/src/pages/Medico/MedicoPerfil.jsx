9d0ed30b1e7dfb0a04bf4489138562a39383220f
Lucyan Ovidio <lucyanovidio@MacBook-Pro-de-Lucyan.local>
2025-06-25 20:36:54 -0300

    feat: integracao perfil admin e medico

--- frontend/src/pages/Medico/MedicoPerfil.jsx
+++ frontend/src/pages/Medico/MedicoPerfil.jsx
@@ -1,15 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./styles.css";
 
 const MedicoPerfil = () => {
-  const consultas = [
-    { id: 1, nome: "Mariana Souza" },
-    { id: 2, nome: "João Pereira" },
-    { id: 3, nome: "Ana Souza" },
-    { id: 4, nome: "Carlos Alberto" },
-    { id: 5, nome: "Joana Medeiros" },
-    { id: 6, nome: "Isabela Silva" },
+    const [usuario, setUsuario] = useState(null);
 
-  ];
+    useEffect(() => {
+        const fetchUsuario = async () => {
+            const token = localStorage.getItem("token");
+
+            try {
+                const response = await axios.get("/api/auth/current", {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                setUsuario(response.data);
+            } catch (error) {
+                console.error("Erro ao buscar dados do usuário:", error);
+            }
+        };
+
+        fetchUsuario();
+    }, []);
 
   return ( 
     <div className="medico-container">
@@ -32,89 +46,45 @@ const MedicoPerfil = () => {
   <div className="perfil-coluna">
     <h3>Informações Pessoais</h3>
 
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Nome Completo</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>Dr. João Souza</p>
-    </div>
+      {usuario ? (
+      
+      <>
+          <div className="perfil-campo">
+              <div className="perfil-campo-header">
+                  <label>Nome Completo</label>
+                  <button className="editar-btn">Editar</button>
+              </div>
+              <p>{usuario.nome}</p>
+          </div>
 
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Email</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>joao.souza@mediagenda.com</p>
-    </div>
+          <div className="perfil-campo">
+              <div className="perfil-campo-header">
+                  <label>Email</label>
+                  <button className="editar-btn">Editar</button>
+              </div>
+              <p>{usuario.email}</p>
+          </div>
 
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Telefone</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>(21) 99999-9999</p>
-    </div>
+          {/* <div className="perfil-campo">
+              <div className="perfil-campo-header">
+                  <label>CPF</label>
+                  <button className="editar-btn">Editar</button>
+              </div>
+              <p>{usuario.cpf}</p>
+          </div> */}
 
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Data de Nascimento</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>15/03/1985</p>
-    </div>
-
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>CPF</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>123.456.789-00</p>
-    </div>
-  </div>
-
-  <div className="perfil-coluna">
-    <h3>Endereço de Atendimento</h3>
-
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>CEP</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>20000-000</p>
-    </div>
-
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Bairro</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>Centro</p>
-    </div>
-
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Rua</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>Rua das Flores</p>
-    </div>
-
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Número</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>463</p>
-    </div>
-
-    <div className="perfil-campo">
-      <div className="perfil-campo-header">
-        <label>Cidade</label>
-        <button className="editar-btn">Editar</button>
-      </div>
-      <p>Rio de Janeiro</p>
-    </div>
+          <div className="perfil-campo">
+              <div className="perfil-campo-header">
+                  <label>Especialidade</label>
+                  <button className="editar-btn">Editar</button>
+              </div>
+              <p>{usuario.especialidade}</p>
+          </div>
+      </>
+      
+      ) : (
+          <h3>Carregando informações do usuário...</h3>
+      )}
   </div>
 </div>
 
@@ -124,10 +94,10 @@ const MedicoPerfil = () => {
 </div>
 
 
-<footer-medico>
+<footer>
     <div>© 2025 MediAgenda</div>
     <div>Contato: contato@mediagenda.com</div>
-</footer-medico>
+</footer>
    
 </main>
 
