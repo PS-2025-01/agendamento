6a16e780c8206b95d0c04de3193f679dd0766325
Carlos Gabriel <83463297+carlosgabriell@users.noreply.github.com>
2025-06-25 22:56:04 -0300

    Update PacienteRoutes.jsx

--- frontend/src/routes/PacienteRoutes.jsx
+++ frontend/src/routes/PacienteRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import PacienteHome from "../pages/Paciente/PacienteHome";
+import PacienteConsultas from "../pages/Paciente/PacienteConsultas";
 import PacientePerfil from "../pages/Paciente/PacientePerfil";
 
 export default function PacienteRoutes() {
   return (
     <Routes>
       <Route path="/paciente/home" element={<PacienteHome />} />
+      <Route path="/paciente/consultas" element={<PacienteConsultas />} />
       <Route path="/paciente/perfil" element={<PacientePerfil />} />
     </Routes>
   );
