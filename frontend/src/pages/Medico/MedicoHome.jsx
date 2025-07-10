a7aa51ca8110a38dcc6c742e2737bdc5714e9919
Mariana Correa <marianacorrea0304@gmail.com>
2025-07-10 14:47:17 -0300

    Status Home Médico

--- frontend/src/pages/Medico/MedicoHome.jsx
+++ frontend/src/pages/Medico/MedicoHome.jsx
@@ -27,6 +27,13 @@ const MedicoHome = () => {
 
   if (!usuario) return <h3>Carregando...</h3>;
 
+  const statusMap = {
+    AGENDADO: "Agendado",
+    CANCELADO: "Cancelado",
+    CONCLUIDO: "Concluido"
+  };
+
+
   return ( 
     <div className="medico-container">
       <Header />
@@ -50,17 +57,22 @@ const MedicoHome = () => {
       <div className="proximas-consultas">
         <h3>Próximas Consultas</h3>
 
-        <div className="consultas-list">
-          {agendamentos.filter(agendamento => agendamento.status == "AGENDADO").map((agendamento) => (
-           <div key={agendamento.id} className="proximas-consultas-wrapper">
-           <div className="proximas-consultas-info">
-             <p>{agendamento.paciente}</p>
-             <p>{new Date(agendamento.data).toLocaleDateString()} - {agendamento.horario}</p>
-         </div>
-         <button className="visualizar-btn">Visualizar</button>
-      </div>
-    ))}
-  </div>
+      <div className="consultas-list">
+         {agendamentos.map((agendamento) => (
+          <div key={agendamento.id} className="proximas-consultas-wrapper">
+            <div className="proximas-consultas-info">
+              <p>{agendamento.paciente}</p>
+              <p>{new Date(agendamento.data).toLocaleDateString()} - {agendamento.horario}</p>
+            </div>
+            
+          <span className={`status-label ${statusMap[agendamento.status].toLowerCase()}`}>
+            {statusMap[agendamento.status]}
+          </span>
+        </div>
+      ))}
+    </div>
+
+
 </div>
 
  <footer className="footer-medico">
